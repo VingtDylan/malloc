1fc2c6d1051a6f0f35458270d34596f544cb1c45
tracer <tracer@tracer.org>
2018-04-13 20:35:13 +0800

    >  compile cyh Linux 161240005 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:46:42 UTC 2018 i686 i686 i686 GNU/Linux

--- malloc.c
+++ malloc.c
@@ -5,10 +5,123 @@
 //=========================================================
 // Your implementations HERE
 //=========================================================
+
+#define BLOCK_SIZE 24
+
+typedef struct block *mblock;
+struct block{
+  size_t size;
+  m_block next;
+  bool free;
+  int padding;
+  char data[1]; 
+}
+
+void *fblock=NULL;
+mblock find_block(mblock *last,size_t size);
+mblock heap_extra(mblock last,size_t size);
+void split(mblock blk,size_t size);
+size_t align8(size_t size);
+void* malloc_unsafe(size_t size);
+
 void* do_malloc(size_t size) {
-    return malloc(size);
+    /*
+    pthread_mutex_lock(&mutex);
+    malloc_unsafe(size);
+    pthread_mutex_unlock(&mutex);
+    */
+    //return malloc(size);
+    return malloc_unsafe(size);
 }
 
 void do_free(void *ptr) {
+    /*
+    pthread_mutex_lock(&mutex);
+    free_unsafe(ptr);
+    pthread_mutex_unlock(&mutex);
+    */
     free(ptr);
 }
+
+mblock find_block(mblock *last,size_t size){
+  mblock b=fblock;
+  while(b&&!(b->free&&b->size>=size)){
+    *last=b;
+    b=b->next;
+  }  
+  return b;
+}
+
+mblock heap_extra(mblock last,size_t size){
+  mblock new;
+  new=sbrk(0);
+  if(sbrk(BLOCK_SIZE+size)==(void*)-1)
+     return NULL:
+  new->size=size;
+  new->next=NULL;
+  if(last)
+     last->next=new;
+  new->free=false;
+  return new;
+}
+
+void split(mblock blk,size_t size){
+  mblock new;
+  new=blk->data+size;
+  new->size=blk->size-size-BLOCK_SIZE;
+  new->next=blk->next;
+  new->free=true;
+  blk->size=size;
+  blk->next=new;
+}
+
+size_t align8(size_t size){
+  if(size&0x7==0)
+     return size;
+  return ((size>>3)+1)<<3;
+}
+
+void* malloc_unsafe(size_t size){
+  mblock blk,last;
+  size_t size;
+  size=align8(size); 
+  if(fblock){
+    last=fblock;
+    blk=find_block(&last,size);
+    if(blk){
+       if((blk->size-size)>=(BLOCK_SIZE+8))
+          split(last,size);
+       blk->free=false;
+    }else{
+       blk=heap_extra(last.size);
+       if(!blk)
+         return NULL;
+    } 
+  }else{
+    blk=heap_extra(NULL,size);
+    if(!blk)
+       return NULL;
+    fblock=b;
+  }
+  return blk->data;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
